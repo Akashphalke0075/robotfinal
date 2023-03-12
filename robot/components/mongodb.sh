b13f0ec53002a1c71b7184c7cd1e48c1c10de09d
Akash <phalke.aka@gmail.com>
2023-03-12 19:21:00 +0530

    update

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -11,8 +11,10 @@ stat $?
 echo -n -e "\e[32minstalling monodb \e[0m:"
 yum install -y $COMPONENT-org  &>> $LOGFILE
 stat $?
+
+echo -n -e "\e[32m open the ip address \e[0m:"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-
-# systemctl enable mongod
-# systemctl start mongod
+echo -n -e "\e[32m starting mongodb \e[0m:"
+systemctl enable mongod
+systemctl start mongod
