50d9b49e2296378c2388b09afee210ae79730446
Akash <phalke.aka@gmail.com>
2023-03-12 14:25:20 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -9,16 +9,21 @@ echo -e "\e[31m please login as root user or sudo \e[0m"
 exit 1
 fi
 
-yum install nginx -y
+echo -e "\e[32m installing nginx \e[0m"
+yum install nginx -y  &>> /tmp/frotnend.log
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
+echo -e "\e[32m downloading component \e[0m"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"  &>> /tmp/frotnend.log
+
+echo -e "\e[32m cleaning up \e[0m"
+rm -rf * /usr/share/nginx/html 
 cd /usr/share/nginx/html
 
+echo -e "\e[32m unzipping component \e[0m"
+unzip /tmp/frontend.zip &>> /tmp/frotnend.log
 
-
-rm -rf *
-unzip /tmp/frontend.zip
+echo -e "\e[32m moving the component \e[0m"
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
