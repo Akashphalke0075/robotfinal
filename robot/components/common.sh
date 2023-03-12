fec8722c986cd5286326413fe8b76e4cf18b9cdc
Akash <phalke.aka@gmail.com>
2023-03-12 15:01:21 +0530

    update

---
+++ robot/components/common.sh
@@ -0,0 +1,15 @@
+LOGFILE=/tmp/frotnend.log
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ] ; then
+echo -e "\e[31m please login as root user or sudo \e[0m"
+exit 1
+fi
+
+stat() {
+if [ $1 -eq 0 ] ; then
+echo -e "\e[32m success \e[0m"
+else
+echo -e "\e[32m failure \e[0m"
+fi
+}
--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -2,21 +2,7 @@
 
 set -e
 COMPONENT=frontend
-LOGFILE=/tmp/frotnend.log
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ] ; then
-echo -e "\e[31m please login as root user or sudo \e[0m"
-exit 1
-fi
-
-stat() {
-if [ $1 -eq 0 ] ; then
-echo -e "\e[32m success \e[0m"
-else
-echo -e "\e[32m failure \e[0m"
-fi
-}
+source components/common.sh
 
 echo -n -e "\e[32m installing nginx \e[0m:"
 yum install nginx -y  &>> $LOGFILE
