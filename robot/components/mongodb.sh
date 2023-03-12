266d8c0fccfc330d98bfd2c7375793962b7628ce
Akash <phalke.aka@gmail.com>
2023-03-12 19:15:35 +0530

    update

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -e
-COMPONENT=frontend
+COMPONENT=mongodb
 source components/common.sh
 
 echo -n -e "\e[32mdownloading componenets \e[0m:"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo   &>> $LOGFILE
+curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo   &>> $LOGFILE
 stat $?
 
 echo -n -e "\e[32minstalling monodb \e[0m:"
-yum install -y mongodb-org  &>> $LOGFILE
+yum install -y $COMPONENT-org  &>> $LOGFILE
 stat $?
 
 # systemctl enable mongod
