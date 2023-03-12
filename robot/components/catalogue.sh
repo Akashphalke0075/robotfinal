2943ef5a7167e250673629197e4824fa4d6f2c6f
Akash <phalke.aka@gmail.com>
2023-03-13 00:29:28 +0530

    update

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -13,8 +13,8 @@ echo -n -e "\e[32m installling nodejs \e[0m:"
 yum install nodejs -y  &>> $LOGFILE
 stat $?
 
-id $APPUSER
-if [ $? -ne 0 ] ; then
+id $APPUSER  &>> $LOGFILE
+if [ $? -ne 0 ]; then
 echo -n -e "\e[32m adding user roboshop \e[0m:"
 useradd $APPUSER
 stat $?
