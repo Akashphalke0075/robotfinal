2a3bed45c0d64a0528b7fc5500ffe963c80495c3
Akash <phalke.aka@gmail.com>
2023-03-12 13:44:09 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ] ; then
+echo -n \e[31m "please login as root user or sudo \e[0m"
+exit 1
 yum install nginx -y
 
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
