4213b4ba77f72e3ab36bb9dde815744079a4ef3d
Akash <phalke.aka@gmail.com>
2023-03-13 17:32:38 +0530

    update

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -4,50 +4,38 @@ COMPONENT=catalogue
 APPUSER=roboshop
 source components/common.sh
 
-echo -n "downloading components:"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
-stat $?
+NODEJS
 
-echo -n "installling nodejs:"
-yum install nodejs -y  &>> $LOGFILE
-stat $?
 
-id $APPUSER  &>> $LOGFILE
-if [ $? -ne 0 ]; then
-echo -n "adding user roboshop:"
-useradd $APPUSER &>> $LOGFILE
-stat $?
-fi
+# echo -n "downloading components:"
+# curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"  &>> $LOGFILE
+# stat $?
 
-echo -n "downloading components:"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"  &>> $LOGFILE
-stat $?
+# echo -n "unzipping moving components:"
+# cd /home/$APPUSER
+# unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
+# rm -rf $COMPONENT
+# mv $COMPONENT-main $COMPONENT
+# stat $?
 
-echo -n "unzipping moving components:"
-cd /home/$APPUSER
-unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
-rm -rf $COMPONENT
-mv $COMPONENT-main $COMPONENT
-cd /home/$APPUSER/$COMPONENT
-stat $?
+# echo -n "installing npm package:"
+# cd /home/$APPUSER/$COMPONENT
+# npm install  &>> $LOGFILE
+# stat $?
 
-echo -n "installing npm package:"
-npm install  &>> $LOGFILE
-stat $?
+# echo -n "chaning permmision:"
+# chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT &>> $LOGFILE
+# stat $?
 
-echo -n "chaning permmision:"
-chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT &>> $LOGFILE
-stat $?
+# echo -n "configuring service name:"
+# sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/' /home/roboshop/$COMPONENT/systemd.service
+# mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+# stat $?
 
-echo -n "configuring service name:"
-sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/' /home/roboshop/$COMPONENT/systemd.service
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $?
-
-echo -n "staring $COMPONENT:"
-systemctl daemon-reload  &>> $LOGFILE
-systemctl start $COMPONENT &>> $LOGFILE
-systemctl enable $COMPONENT &>> $LOGFILE
-stat $?
+# echo -n "staring $COMPONENT:"
+# systemctl daemon-reload  &>> $LOGFILE
+# systemctl start $COMPONENT &>> $LOGFILE
+# systemctl enable $COMPONENT &>> $LOGFILE
+# stat $?
 
 echo -n "_____installation completed________"
--- robot/components/common.sh
+++ robot/components/common.sh
@@ -13,3 +13,67 @@ else
 echo -e "\e[31m failure \e[0m"
 fi
 }
+
+NODEJS() {
+echo -n "downloading components:"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
+stat $?
+
+echo -n "installling nodejs:"
+yum install nodejs -y  &>> $LOGFILE
+stat $?
+#CALLING CRETE FINCTION
+CREATE_USER
+
+DOWNLOAD_EXTARCT
+
+NPM_INSTALL
+
+CONFIGURE_SERVICE
+
+}
+
+CREATE_USER() {
+id $APPUSER  &>> $LOGFILE
+if [ $? -ne 0 ]; then
+echo -n "adding user roboshop:"
+useradd $APPUSER &>> $LOGFILE
+stat $?
+fi
+}
+
+DOWNLOAD_EXTARCT(){
+echo -n "downloading components:"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"  &>> $LOGFILE
+stat $?
+
+echo -n "performing cleanup"
+rm -rf /home/$APPUSER/$COMPONENT
+cd /home/$APPUSER
+unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE && mv $COMPONENT-main $COMPONENT  &>> $LOGFILE
+stat $?
+
+echo -n "chaning permmision:"
+chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT &>> $LOGFILE
+stat $?
+}
+
+NPM_INSTALL() {
+echo -n "installing npm package:"
+cd $COMPONENT
+npm install  &>> $LOGFILE
+stat $?
+}
+
+CONFIGURE_SERVICE() {
+echo -n "configuring service name:"
+sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/' /home/roboshop/$COMPONENT/systemd.service
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+stat $?
+
+echo -n "staring $COMPONENT:"
+systemctl daemon-reload  &>> $LOGFILE
+systemctl start $COMPONENT &>> $LOGFILE
+systemctl enable $COMPONENT &>> $LOGFILE
+stat $?
+}
