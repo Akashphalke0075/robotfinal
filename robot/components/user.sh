a4a9c27a5dcd8980de26304fbdd2b074f5105024
Akash <phalke.aka@gmail.com>
2023-03-13 15:24:03 +0530

    update

--- robot/components/user.sh
+++ robot/components/user.sh
@@ -46,8 +46,8 @@ stat $?
 
 echo -n "staring $COMPONENT:"
 systemctl daemon-reload  &>> $LOGFILE
-systemctl start catalogue &>> $LOGFILE
 systemctl enable catalogue &>> $LOGFILE
+systemctl start catalogue &>> $LOGFILE
 stat $?
 
 echo -n "_____installation completed________"
