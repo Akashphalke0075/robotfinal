51f807bd316ff5fd36101f735e2bc858e2c24645
Akash <phalke.aka@gmail.com>
2023-03-13 14:56:00 +0530

    update

---
+++ robot/components/redis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+COMPONENT=redis
+APPUSER=roboshop
+source components/common.sh
+
+echo -n "downloading components:"
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo  &>> $LOGFILE 
+stat $?
+
+echo -n "installation redis:"
+yum install redis-6.2.11 -y  &>> $LOGFILE
+stat $?
+
+echo -n "whitelisting redis:"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf 
+stat $?
+
+echo -n " starting redis :"
+systemctl daemon-reload  &>> $LOGFILE
+systemctl enable redis  &>> $LOGFILE
+systemctl start redis   &>> $LOGFILE
+$?
