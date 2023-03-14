afa4ac6ba5f5d3d933627a25dfe2546e3ea59f5f
Akash <phalke.aka@gmail.com>
2023-03-14 11:01:43 +0530

    update

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -3,6 +3,8 @@
 COMPONENT=mysql
 source components/common.sh
 
+read -p 'enter my sql password:' MYSQL_PWD
+
 echo -n "downloading component:"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo   &>> $LOGFILE
 stat $?
@@ -16,3 +18,20 @@ systemctl enable mysqld  &>> $LOGFILE
 systemctl start mysqld  &>> $LOGFILE
 stat $?
 
+echo -n "temporary pssword:"
+DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
+
+
+echo show databases | mysql -uroot p${MYSQL_PWD}  &>> $LOGFILE
+if [ $? -ne 0 ]; then
+echo -n "reset root password"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PWD}';" | mysql   --connect-expired-password -uroot -p"{DEF_ROOT_PASSWORD}" &>> $LOGFILE
+stat $?
+fi
+
+echo show plugins | mysql -uroot p${MYSQL_PWD} | grep validate_password; &>> $LOGFILE
+if [ $? -eq 0 ]; then
+echo -n "uninstalling password validate plugin"
+echo "uninstall plugin validate_password;" | mysql   --connect-expired-password -uroot -p${MYSQL_PWD} &>> $LOGFILE
+stat $?
+fi
